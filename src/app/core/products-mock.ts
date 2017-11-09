dec5ed10c199d8c179c7cdcf372c5c759b6216ef
Ilya Ilin <ii@bekitzur.com>
2017-11-09 15:00:08 +0300

    feat: center product list content

--- src/app/core/products-mock.ts
+++ src/app/core/products-mock.ts
@@ -6,4 +6,32 @@ export const products = [
     purchases: 3000,
     rating: 65,
   },
+  {
+    id: 2,
+    name: 'Test product',
+    price: 200,
+    purchases: 3000,
+    rating: 65,
+  },
+  {
+    id: 3,
+    name: 'Test product',
+    price: 200,
+    purchases: 3000,
+    rating: 65,
+  },
+  {
+    id: 4,
+    name: 'Test product',
+    price: 200,
+    purchases: 3000,
+    rating: 65,
+  },
+  {
+    id: 5,
+    name: 'Test product',
+    price: 200,
+    purchases: 3000,
+    rating: 65,
+  },
 ];
